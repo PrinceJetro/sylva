23df6f8cac434de70ba9f3a273dd1275b485c40e
Adegbuyi Jephthah <adegbuyijephthah@gmail.com>
2023-10-17 01:07:30 +0100

    made it work on mobile only

--- src/index.js
+++ src/index.js
@@ -4,14 +4,31 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
+function checkScreenWidth() {
+  const minWidth = 450; // Define the minimum width
+
+  if (window.innerWidth < minWidth) {
+    // Check if the screen width is less than the minimum width
+    // Render your React app
+    const root = ReactDOM.createRoot(document.getElementById('root'));
+    root.render(
+    <React.StrictMode>
     <App />
   </React.StrictMode>
 );
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
+  } else {
+    // Handle the case where the screen width is too wide
+    const root = ReactDOM.createRoot(document.getElementById('root'));
+    root.render(
+      <React.StrictMode>
+        <h1>This App is for mobile only </h1>
+      </React.StrictMode>
+    );
+    
+  }
+}
+
+// Call the function when the page loads
+checkScreenWidth();
 reportWebVitals();
